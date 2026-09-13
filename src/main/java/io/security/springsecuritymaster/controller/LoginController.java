72bb069ae2ed7b10c567fb7b98bd632bb451c0ed
devwooo <devacnt25@gmail.com>
2026-09-13 10:29:32 +0900

    ë¬anonymouse 커밋

--- src/main/java/io/security/springsecuritymaster/controller/LoginController.java
+++ src/main/java/io/security/springsecuritymaster/controller/LoginController.java
@@ -1,5 +1,9 @@
 package io.security.springsecuritymaster.controller;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,5 +25,22 @@ public class LoginController {
 	public String loginPost() {
 		System.out.println("Login Post");
 		return "index";
+	}
+
+	@GetMapping("/anonymouse")
+	public String anonymouse() { return "anonymouse"; }
+
+	@GetMapping("/authenticatoin")
+	public String authenticatoin(Authentication authentication) {
+		if (authentication instanceof AnonymousAuthenticationToken) {
+			return "anonymouse";
+		} else {
+			return "null";
+		}
+	}
+
+	@GetMapping("/anonymouseContext")
+	public String anonymouseContext(@CurrentSecurityContext SecurityContext context) {
+		return context.getAuthentication().getName();
 	}
 }
